1f7fd59070f2f0e90553663742cd31066345753f
paulo1998dantas-crypto <paulo1998dantas-crypto@users.noreply.github.com>
2026-05-26 00:34:55 -0300

    Ajustar pool SQLAlchemy para Supabase

--- estoque_app/database.py
+++ estoque_app/database.py
@@ -1,7 +1,9 @@
 from contextlib import contextmanager
+from urllib.parse import urlsplit
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, scoped_session, sessionmaker
+from sqlalchemy.pool import NullPool
 
 from config import Config
 
@@ -14,10 +16,14 @@ engine_kwargs = {
 if Config.SQLALCHEMY_DATABASE_URI.startswith("sqlite"):
     engine_kwargs["connect_args"] = {"check_same_thread": False}
 else:
-    engine_kwargs["pool_size"] = 5
-    engine_kwargs["max_overflow"] = 5
-    engine_kwargs["pool_recycle"] = 1800
     engine_kwargs["connect_args"] = {"prepare_threshold": None}
+    db_port = urlsplit(Config.SQLALCHEMY_DATABASE_URI).port
+    if db_port == 6543:
+        engine_kwargs["poolclass"] = NullPool
+    else:
+        engine_kwargs["pool_size"] = 5
+        engine_kwargs["max_overflow"] = 5
+        engine_kwargs["pool_recycle"] = 1800
 
 engine = create_engine(Config.SQLALCHEMY_DATABASE_URI, **engine_kwargs)
 SessionLocal = scoped_session(
